29cdf071b8a76c397d550c99f947b16840ba0fa3
Lev <lev.orekhov@gmail.com>
2012-06-28 11:09:33 +0600

    models finished, methods for load balancin were added

--- urlobserver/core/models.py
+++ urlobserver/core/models.py
@@ -20,3 +20,17 @@ class Url(models.Model):
 
 class Worker(models.Model):
     url = models.CharField(max_length=1000)
+
+
+def get_urls_to_check():
+    '''
+    We need to get urls by chunks to minimize load on yandex.market/
+    '''
+    return Url.objects.all()
+
+def get_workers():
+    '''
+    We need to balance load between workers,
+    as we want them to serve as long as possible.
+    '''
+    return Worker.objects.all()
