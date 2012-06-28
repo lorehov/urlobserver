60fbfbfcef235f672f34e0c47b4e12574101053a
Roman Imankulov <roman@netangels.ru>
2012-06-28 13:16:22 +0600

    Created urlobservers.core.make_request

--- urlobserver/core/utils.py
+++ urlobserver/core/utils.py
@@ -1,5 +1,15 @@
-from urlobserver.core import models
+# -*- coding: utf-8 -*-
+import urllib
 
+
+def make_request(worker, remote_url):
+    proxies = {'http': 'http://%s:8888' % worker.domain_name}
+    fd = urllib.urlopen(remote_url, proxies=proxies)
+    return {
+        'content': fd.read(),
+        'status_code': fd.getcode(),
+        'instance_id': worker.instance_id,
+    }
 
 def update_urls():
     pass
