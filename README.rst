1607a6b94e1d49b676feda44887792f508c67c81
Lev <lev.orekhov@gmail.com>
2012-06-28 10:26:47 +0600

    Initial commit. Contains project sceleton only.

---
+++ README.rst
@@ -0,0 +1,9 @@
+This is my own simple template for Django projects greater or equal than 1.4
+
+Quickstart::
+
+    $ django-admin.py startproject foo --template <...>/django-template
+    $ cd foo
+    $ ./manage.py syncdb --noinput
+    $ ./manage.py runserver
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='urlobserver',
+    version='0.0',
+    packages=find_packages(),
+    author='urlobserver author',
+    author_email='me@example.com',
+    license='BSD',
+    description='urlobserver description',
+    scripts=['manage.py',],
+)
